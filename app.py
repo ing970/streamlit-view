2ee9b8f086628d6ce056c45cd2536fbc6165ec30
SeokminLEE <120775224+ing970@users.noreply.github.com>
2023-05-04 15:07:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -58,18 +58,3 @@ col1, col2, col3, = st.columns(3)
 col1.metric(label = "달러USD", value = "1,228 원", delta = "-12.00 원")
 col2.metric(label = "일본JPY(100엔)", value = "958.63 원", delta = "-7.44 원")
 col3.metric(label = "유럽연합EUR", value = "1,335.82 원", delta = "11.44 원")
-
-
-
-
-
-# view = [100, 150, 30]
-# seview = pd.Series(view)
-
-# st.write("# Youtube view")
-# st.write("## 1. raw")
-# view
-# st.write("## 2. Bar_chart")
-# st.bar_chart(view)
-# st.write("## 3. Series")
-# seview
