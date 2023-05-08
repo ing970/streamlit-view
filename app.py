40cfbcb2bc8ec52ed84c18e20dcb7077cb74b95e
SeokminLEE <120775224+ing970@users.noreply.github.com>
2023-05-08 09:28:10 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -58,3 +58,5 @@ col1, col2, col3, = st.columns(3)
 col1.metric(label = "달러USD", value = "1,228 원", delta = "-12.00 원")
 col2.metric(label = "일본JPY(100엔)", value = "958.63 원", delta = "-7.44 원")
 col3.metric(label = "유럽연합EUR", value = "1,335.82 원", delta = "11.44 원")
+
+st.markdown("추가할 사항+")
